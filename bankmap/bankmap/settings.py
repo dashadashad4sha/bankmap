a8616c8bba59ed2ddfda8b87329a8077d0ed5098
daria <daria.matveeva@list.ru>
2023-10-05 12:25:59 +0500

    add dock

--- bankmap/bankmap/settings.py
+++ bankmap/bankmap/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
 
     'banks.apps.BanksConfig',
 ]
--- bankmap/bankmap/urls.py
+++ bankmap/bankmap/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from .settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('banks.urls'))
+    path('api/', include('banks.urls')),
+    path('test/', include('banks.urls')),
 ]
 
+urlpatterns += doc_urls
 
 if DEBUG:
     urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
