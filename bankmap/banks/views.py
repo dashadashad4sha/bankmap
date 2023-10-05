be5087f988f277cac0228db79873cc8f6417c6ca
daria <daria.matveeva@list.ru>
2023-10-05 12:31:59 +0500

    add descriptions

--- bankmap/banks/views.py
+++ bankmap/banks/views.py
@@ -6,15 +6,18 @@ from banks.serializers import BankSerializer, WorkloadSerializer
 
 
 class BankAPIView(generics.ListAPIView):
+    """Вывод списка всех отделений"""
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
 
 class BankData(generics.RetrieveAPIView):
+    """Вывод информации об отделении по индексу"""
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
 
 class WorkloadAPIView(generics.ListAPIView):
+    """Вывод списка загруженностей"""
     queryset = Workload.objects.all()
     serializer_class = WorkloadSerializer
